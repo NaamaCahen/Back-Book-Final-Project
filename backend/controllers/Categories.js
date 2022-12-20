d1fdf3741fc383d24c53e497f2b8c3b60f12bb02
NaamaCahen <cahenbb@gmail.com>
2022-12-20 13:04:31 +0200

    get statuses

--- backend/controllers/Categories.js
+++ backend/controllers/Categories.js
@@ -17,3 +17,21 @@ export const getAges=(req,res)=>{
         res.status(404).json({msg:e.message});
     })
 }
+
+export const getBookStatus=(req,res)=>{
+    db('books_status')
+    .select('*')
+    .then(rows=>res.json(rows))
+    .catch(e=>{
+        res.status(404).json({msg:e.message});
+    })
+}
+
+export const getAssigningStatus=(req,res)=>{
+    db('assigning_status')
+    .select('*')
+    .then(rows=>res.json(rows))
+    .catch(e=>{
+        res.status(404).json({msg:e.message});
+    })
+}
--- backend/routes/Categories.js
+++ backend/routes/Categories.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { getAges, getCategories } from "../controllers/Categories.js";
+import { getAges, getAssigningStatus, getBookStatus, getCategories } from "../controllers/Categories.js";
 
 export const router=express.Router();
 
 router.get('/categories',getCategories)
 router.get('/ages',getAges)
+router.get('/ages',getBookStatus)
+router.get('/ages',getAssigningStatus)
