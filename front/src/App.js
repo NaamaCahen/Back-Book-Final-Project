ba75d731dd93088351e1da4db61e0d5a7aabf99c
NaamaCahen <cahenbb@gmail.com>
2022-12-20 21:17:30 +0200

    navbar

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    
-      <Navbar />
-      <Login />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/myBooks' element={<Login/>}/>
+          <Route path='/search' element={<Login/>}/>
+        </Routes>
+        
+      </BrowserRouter>
     </>
   );
 }
