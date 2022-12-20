26b17f8af3fceb125099e45ab03fa690b92f91ee
NaamaCahen <cahenbb@gmail.com>
2022-12-20 20:10:23 +0200

    creating react app , installing tailwind and making a navbar

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+    
+      <Navbar />
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ front/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+     Login
+    </h1>
+    </>
+  )
+}
+
+export default Login
