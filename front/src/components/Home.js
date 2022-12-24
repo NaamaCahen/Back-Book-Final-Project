7adf7137fe45fc02bfdc50386ace8a084c94362f
NaamaCahen <cahenbb@gmail.com>
2022-12-25 00:46:38 +0200

    fetching the books with redux

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -2,14 +2,17 @@ import React from 'react'
 // import Map from './Map'
 import {MyMapComponent} from './Map';
 import { useState,useEffect } from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import { count, fetchBooks } from '../redux/booksSlice';
 import jwt_decode from 'jwt-decode';
 
 function Home() {
-    const [books,setBooks]=useState([])
+    const books=useSelector((state)=>state.booksReducer.booksArr)
+    const dispatch=useDispatch();
     useEffect(()=>{
         const decode=jwt_decode(localStorage.getItem('token'));
         console.log(decode);
-       
+       dispatch(fetchBooks())
     },[])
     return (
         <>
--- front/src/redux/booksSlice.js
+++ front/src/redux/booksSlice.js
@@ -1,25 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState={
-    books:[]
+    booksArr:[],
+    error:''
 }
+
+export const fetchBooks = createAsyncThunk(
+    'books/fetchBooksStatus',
+    () => {
+      fetch(`http://localhost:4000/books`)
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data);
+        return data;
+      })
+    }
+  )
+  
+
 
 export const booksSlice=createSlice({
     name:'books',
     initialState,
-    reducers:{
-        getBooks(state,action){
-            state.books=action.payload
-        }
+    
+    extraReducers:(builder)=>{
+        builder.addCase(fetchBooks.fulfilled,(state,action)=>{
+            state.booksArr=action.payload
+            state.error=''
+        })
+        builder.addCase(fetchBooks.rejected,(state,action)=>{
+            state.booksArr=[]
+            state.error=action.error.message
+        })
     }
 })
 
 // Destructure and export the plain action creators
-export const {getBooks}=booksSlice.actions;
-
-const fetchBooks=()=> async(dispatch)=>{
-    const response=await fetch(`http://localhost:4000/books`).json()
-    console.log(response);
-    dispatch(getBooks(response.data))
-}
+export const {getBooks,count}=booksSlice.actions;
  
+export default booksSlice.reducer;
