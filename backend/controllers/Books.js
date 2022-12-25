88fa140f07b15222c6b94bf2208e8e63100bf64a
NaamaCahen <cahenbb@gmail.com>
2022-12-25 23:38:21 +0200

    api for fetching all the books details

--- backend/controllers/Books.js
+++ backend/controllers/Books.js
@@ -1,6 +1,6 @@
 import db from "../config/elephantsql.js";
 
-export const getBooks =async  (req, res) => {
+export const getBooks = async (req, res) => {
     // try {
     //     const rows = await db('books').select('*');
     //    res.json(rows);
@@ -9,41 +9,68 @@ export const getBooks =async  (req, res) => {
     // }
 
     db('books')
-    .join('book_assigning','books.book_id','=','book_assigning.book_id')
-    .join('users','users.user_id','=','book_assigning.user_id')
-    .select('*')
-    .then(rows=>res.json(rows))
-    .catch(e=>{
-        console.log(e);
-        res.status(404).json({msg:e.message});
-    })
+        .join('book_assigning', 'books.book_id', '=', 'book_assigning.book_id')
+        .join('users', 'users.user_id', '=', 'book_assigning.user_id')
+        .join('categories', 'books.category', '=', 'categories.category_id')
+        .join('ages', 'books.age', '=', 'ages.age_id')
+        .join('assigning_status','assigning_status.status_id','=','book_assigning.status')
+        .join('books_status','books_status.status_id','=','books.book_status')
+        .select(
+            'books.book_id',
+            'books.title',
+            'books.author_first_name',
+            'books.author_last_name',
+            'books_status.status_name',
+            'categories.category_name',
+            'ages.age_description',
+            'book_assigning.book_assigning_id',
+            'users.user_id',
+            'assigning_status.status_name',
+            'book_assigning.requestedat',
+            'book_assigning.receivedat',
+            'book_assigning.givenat',
+            'book_assigning.addedat',
+            'users.email',
+            'users.user_first_name',
+            'users.user_last_name',
+            'users.country',
+            'users.city',
+            'users.street',
+            'users.num_house',
+            'users.phone'
+        )
+        .then(rows => res.json(rows))
+        .catch(e => {
+            console.log(e);
+            res.status(404).json({ msg: e.message });
+        })
 
 }
 
 //add a new book (adding also a row in books_assignment)
-export const newBook=(req,res)=>{
+export const newBook = (req, res) => {
     let book_id;
-    const {title,author_first_name,author_last_name,category,book_status,age,user_id}=req.body;
+    const { title, author_first_name, author_last_name, category, book_status, age, user_id } = req.body;
     db('books')
-    .insert(title,author_first_name,author_last_name,category,book_status,age)
-    .returning('*')
-    .then(rows=>{
-        book_id=rows[0].book_id;
-        res.json(rows)
-    })
-    .catch(e=>res.status(404).json({msg:e.message}));
+        .insert(title, author_first_name, author_last_name, category, book_status, age)
+        .returning('*')
+        .then(rows => {
+            book_id = rows[0].book_id;
+            res.json(rows)
+        })
+        .catch(e => res.status(404).json({ msg: e.message }));
 
-    newBookAssigning(book_id,user_id,4,new Date());
+    newBookAssigning(book_id, user_id, 4, new Date());
 }
 
-const newBookAssigning=(book_id,user_id,status,date)=>{
-    let day=date.getDay();
-    day<10?day='0'+day:day;
-    let month=date.getMonth();
-    month<10?month='0'+month:month;
-    const year=date.getFullYear();
-    const addedat=`${year}-${month}-${day}`
+const newBookAssigning = (book_id, user_id, status, date) => {
+    let day = date.getDay();
+    day < 10 ? day = '0' + day : day;
+    let month = date.getMonth();
+    month < 10 ? month = '0' + month : month;
+    const year = date.getFullYear();
+    const addedat = `${year}-${month}-${day}`
     db('book_assigning')
-    .insert(user_id,book_id,status,addedat)
-    
+        .insert(user_id, book_id, status, addedat)
+
 }
