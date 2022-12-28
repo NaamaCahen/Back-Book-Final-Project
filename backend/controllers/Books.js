086de7007f814ac29c0217f0006fdec3684fa69d
NaamaCahen <cahenbb@gmail.com>
2022-12-28 14:25:18 +0200

    modifying the add book api (separating to 2 apis)

--- backend/controllers/Books.js
+++ backend/controllers/Books.js
@@ -43,34 +43,33 @@ export const getBooks = async (req, res) => {
 
 //add a new book (adding also a row in books_assignment)
 export const newBook = (req, res) => {
-    let book_id;
     const { title, author_first_name, author_last_name, category, book_status, age, user_id } = req.body;
     db('books')
-        .insert(title, author_first_name, author_last_name, category, book_status, age)
+        .insert({ title, author_first_name, author_last_name, category, book_status, age })
         .returning('*')
         .then(rows => {
-            book_id = rows[0].book_id;
-            res.json(rows)
+            console.log('rowss', rows);
+
+            res.json(rows[0])
         })
         .catch(e => res.status(404).json({ msg: e.message }));
 
-    newBookAssigning(book_id, user_id, 4, new Date());
+
 }
 
-const newBookAssigning = (book_id, user_id, status, date) => {
-    let day = date.getDay();
-    day < 10 ? day = '0' + day : day;
-    let month = date.getMonth();
-    month < 10 ? month = '0' + month : month;
-    const year = date.getFullYear();
-    const addedat = `${year}-${month}-${day}`
+export const newBookAssigning = (req, res) => {
+    const { book_id, user_id, status, addedat } = req.body;
+    
     db('book_assigning')
-        .insert(user_id, book_id, status, addedat)
+        .insert({ user_id, book_id, status, addedat })
+        .returning('*')
+        .then(rows=>res.json(rows[0]))
+        .catch(e=>res.status(404).json({msg:e.message}));
 
 }
 
 export const getMyBooks = (req, res) => {
-    const {id}=req.params;
+    const { id } = req.params;
     db('books')
         .join('book_assigning', 'books.book_id', '=', 'book_assigning.book_id')
         .join('users', 'users.user_id', '=', 'book_assigning.user_id')
@@ -87,7 +86,7 @@ export const getMyBooks = (req, res) => {
             'categories.category_name',
             'ages.age_description',
             'book_assigning.book_assigning_id',
-            'users.user_id' ,
+            'users.user_id',
             'assigning_status.status_name',
             'book_assigning.requestedat',
             'book_assigning.receivedat',
@@ -102,7 +101,7 @@ export const getMyBooks = (req, res) => {
             'users.num_house',
             'users.phone'
         )
-        .where({'users.user_id':id})
+        .where({ 'users.user_id': id })
         .then(rows => res.json(rows))
         .catch(e => {
             console.log(e);
--- backend/routes/Books.js
+++ backend/routes/Books.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getBooks, newBook,getMyBooks } from '../controllers/Books.js';
+import { getBooks, newBook,getMyBooks, newBookAssigning } from '../controllers/Books.js';
 import { VerifyToken } from '../middlewares/VerifyToken.js';
 
 export const booksRouter=express.Router();
 
 booksRouter.get('/books', getBooks);
 booksRouter.post('/books',newBook);
-booksRouter.get('/books/:id',getMyBooks)
+booksRouter.post('/bookAssign',newBookAssigning);
+booksRouter.get('/books/:id',getMyBooks);
