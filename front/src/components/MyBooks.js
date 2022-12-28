1b7047e6b5f73068fba2f90137c850ad58271da6
NaamaCahen <cahenbb@gmail.com>
2022-12-28 11:29:40 +0200

    my books table

--- front/src/components/MyBooks.js
+++ front/src/components/MyBooks.js
@@ -1,33 +1,34 @@
 import React, { useEffect } from 'react'
-import {Button, Tabs} from 'flowbite-react';
-import {useDispatch,useSelector} from 'react-redux'
+import { Button, Table, Tabs } from 'flowbite-react';
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchMyBooks } from '../redux/booksSlice';
+import BooksTable from './BooksTable';
 
 function MyBooks() {
-  const dispatch=useDispatch();
-  const myBooks=useSelector(state=>state.books.myBooks);
-  const user=useSelector(state=>state.users.user);
-  useEffect(()=>{
+  const dispatch = useDispatch();
+  const myBooks = useSelector(state => state.books.myBooks);
+  const user = useSelector(state => state.users.user);
+  useEffect(() => {
     dispatch(fetchMyBooks(user.user_id))
-  },[])
+  }, [])
   return (
     <>
       <h1>MyBooks</h1>
       <div className='h-96'>
-      <Tabs.Group aria-label="Tabs with underline" style="underline">
-        <Tabs.Item title="sharing">
-          sharing
-        </Tabs.Item>
-        <Tabs.Item title="reading">
-          reading
-        </Tabs.Item>
-        <Tabs.Item title="requested">
-          requested
-        </Tabs.Item>
-        <Tabs.Item title="given">
-          given
-        </Tabs.Item>
-      </Tabs.Group>
+        <Tabs.Group aria-label="Tabs with underline" style="underline">
+          <Tabs.Item title="sharing">
+            <BooksTable tab='shared' filtered={myBooks.filter(item=>{ return item.status_name === 'added' || (item.status_name === 'received' && item.status_description === 'for sharing') })}/>
+          </Tabs.Item>
+          <Tabs.Item title="reading">
+            <BooksTable tab='received' filtered={myBooks.filter(item=>item.status_name==='received')}/>
+          </Tabs.Item>
+          <Tabs.Item title="requested">
+            <BooksTable tab='requested' filtered={myBooks.filter(item=>item.status_name==='requested')}/>
+          </Tabs.Item>
+          <Tabs.Item title="given">
+            <BooksTable tab='given' filtered={myBooks.filter(item=>item.status_name==='given')}/>
+          </Tabs.Item>
+        </Tabs.Group>
       </div>
       <Button>add new book</Button>
     </>
