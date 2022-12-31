a358791f6b019b0ea3e12c3290e9612986dc88e2
NaamaCahen <cahenbb@gmail.com>
2022-12-31 20:52:19 +0200

    refetch after approving a request

--- front/src/components/MyRequests.js
+++ front/src/components/MyRequests.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchMyRequests } from '../redux/assigningSlice';
-import { Alert, Button,Modal } from 'flowbite-react';
-import {HiOutlineExclamationCircle} from 'react-icons/hi'
+import {fetchMyBooks} from '../redux/booksSlice'
+import { Alert, Button, Modal } from 'flowbite-react';
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 
 function MyRequests() {
   const dispatch = useDispatch();
@@ -10,33 +11,42 @@ function MyRequests() {
   const user = useSelector(state => state.users.user)
   const myBooks = useSelector(state => state.books.myBooks)
   const [details, setDetails] = useState(false) //modal to display more details
-  const [openModal,setOpenModal]=useState(false);
-  const [numRequest,setNumRequest]=useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  const [numRequest, setNumRequest] = useState(0);
+  const [refetch,setRefetch]=useState();
 
   useEffect(() => {
     dispatch(fetchMyRequests(user.user_id))
   }, [])
 
-  const give=()=>{
+  useEffect(()=>{
+    dispatch(fetchMyBooks(user.user_id))
+    dispatch(fetchMyRequests(user.user_id))
+  },[refetch])
+
+  const give = () => {
     setOpenModal(false);
-    fetch(`/acceptReq`,{
-      method:'POST',
-      headers:{
-        'Content-Type':'Application/json'
+    fetch(`/acceptReq`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json'
       },
-      body:JSON.stringify({
-        book_assigning_id:myRequests[numRequest].book_assigning_id,
-        receivedat:new Date().toISOString().split('T')[0],
-        from_user:myRequests[numRequest].from_user,
+      body: JSON.stringify({
+        book_assigning_id: myRequests[numRequest].book_assigning_id,
+        receivedat: new Date().toISOString().split('T')[0],
+        from_user: myRequests[numRequest].from_user,
       })
-    }).then(res=>res.json())
-    .then(msg=>console.log(msg))
-    .catch(e=>console.log(e))
+    }).then(res => res.json())
+      .then(msg => {
+        console.log(msg)
+        setRefetch(msg)
+      })
+      .catch(e => console.log(e))
   }
 
   return (
     <>
-      <h1>your books are requested!</h1>
+      {myRequests.length > 0 ? <h1>your books are requested!</h1> : null}
       {myRequests.length > 0 ?
         myRequests.map(item => {
           return (
@@ -48,13 +58,13 @@ function MyRequests() {
                   </span>
                   <div className='flex '>
                     <Button outline={true} onClick={() => setDetails(true)}>more details</Button>
-                    <Button onClick={()=>setOpenModal(true)}>give</Button>
+                    <Button onClick={() => setOpenModal(true)}>give</Button>
                   </div>
                 </span>
               </Alert>
 
-            {/* //details modal */}
-              <Modal show={details} onClose={()=>setDetails(false)}>
+              {/* //details modal */}
+              <Modal show={details} onClose={() => setDetails(false)}>
                 <Modal.Header>
                   details
                 </Modal.Header>
@@ -64,30 +74,30 @@ function MyRequests() {
                   phone:
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button onClick={()=>setDetails(false)}>ok</Button>
+                  <Button onClick={() => setDetails(false)}>ok</Button>
                 </Modal.Footer>
               </Modal>
 
               {/* are you sure modal */}
               <Modal show={openModal} popup={true} onClose={() => setOpenModal(false)}>
-                    <Modal.Header />
-                    <Modal.Body>
-                        <div className="text-center">
-                            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-                            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                Are you sure you want to give this book?
-                            </h3>
-                            <div className="flex justify-center gap-4">
-                                <Button gradientDuoTone='purpleToBlue' onClick={give}>
-                                    Yes, I'm sure
-                                </Button>
-                                <Button color="gray" onClick={() => setOpenModal(false)}>
-                                    No, cancel
-                                </Button>
-                            </div>
-                        </div>
-                    </Modal.Body>
-                </Modal>
+                <Modal.Header />
+                <Modal.Body>
+                  <div className="text-center">
+                    <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                      Are you sure you want to give this book?
+                    </h3>
+                    <div className="flex justify-center gap-4">
+                      <Button gradientDuoTone='purpleToBlue' onClick={give}>
+                        Yes, I'm sure
+                      </Button>
+                      <Button color="gray" onClick={() => setOpenModal(false)}>
+                        No, cancel
+                      </Button>
+                    </div>
+                  </div>
+                </Modal.Body>
+              </Modal>
             </>
           )
         }) : 'no requests for now'
