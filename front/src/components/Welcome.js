efb19851b31d79dd656a777f4a59329db8d5c3ff
NaamaCahen <cahenbb@gmail.com>
2023-01-01 05:59:25 +0200

    style welcome page

--- front/src/components/Welcome.js
+++ front/src/components/Welcome.js
@@ -1,13 +1,20 @@
+import { Button } from 'flowbite-react'
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import logo from '../logo/logobg.png'
 
 function Welcome() {
     return (
         <>
-            <h1>Welcome</h1>
-            <Link to='/login'>login</Link>
-            <br/>
-            <Link to='/register'>register</Link>
+            <div className='bg-gradient-to-r from-cyan-500 to-blue-500 h-screen'>
+                <img src={logo} alt='logo' />
+                <div className='flex justify-center '>
+                    <Button className='m-5' gradientDuoTone='purpleToBlue'><Link className='text-xl font-bold ' to='/login'>login</Link></Button>
+                    <Button className='m-5' gradientDuoTone='purpleToBlue'><Link className='text-xl font-bold ' to='/register'>register</Link></Button>
+                </div>
+
+            </div>
+
         </>
 
     )
