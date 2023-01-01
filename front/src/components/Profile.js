ca20986f8a5cb277a3d1d0cbafc3e74ab39fd1ba
NaamaCahen <cahenbb@gmail.com>
2023-01-01 07:14:30 +0200

    style profile

--- front/src/components/Profile.js
+++ front/src/components/Profile.js
@@ -1,12 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Modal } from 'flowbite-react'
-import React, { useEffect ,useState} from "react";
+import { Button, Label, Modal, TextInput, Card } from 'flowbite-react'
+import React, { useEffect, useState } from "react";
 
 
 const Profile = () => {
     const user = useSelector(state => state.users.user);
     const dispatch = useDispatch();
-    const [openModal,setOpenModal]=useState(false);
+    const [openModal, setOpenModal] = useState(false);
 
 
     const save = (e) => {
@@ -54,40 +54,44 @@ const Profile = () => {
 
     return (
         <>
-            <h1>Your Profile:</h1>
-            <form onSubmit={save}>
-                <label htmlFor='firstName'>first name</label>
-                <input id="firstName" name='firstName' type='text' defaultValue={user.user_first_name} required/>
+            <Card className="sm:w-full md:w-1/2 lg:w-1/3 mt-20 m-auto">
+                <h1 className="text-2xl font-bold">Your Profile:</h1>
+                <form onSubmit={save}>
+                    <Label htmlFor='firstName' value='first name' />
+                    <TextInput id="firstName" name='firstName' type='text' defaultValue={user.user_first_name} required />
 
-                <label htmlFor='lastName'>last name</label>
-                <input id="lastName" name='lastName' type='text' defaultValue={user.user_last_name} required/>
+                    <Label htmlFor='lastName' value='last name' />
+                    <TextInput id="lastName" name='lastName' type='text' defaultValue={user.user_last_name} required />
 
-                <label htmlFor='email'>email</label>
-                <input id="email" name='email' type='email' defaultValue={user.email} required/>
+                    <Label htmlFor='email' value='email' />
+                    <TextInput id="email" name='email' type='email' defaultValue={user.email} required />
 
-                {/* //add the possibility to change the password */}
-                {/* <label htmlFor='password'>password</label>
-                <input id="password" name='password' type='text' defaultValue={user.password}  /> */}
+                    {/* //add the possibility to change the password */}
+                    {/* <label htmlFor='password'>password</label>
+                    <TextInput id="password" name='password' type='text' defaultValue={user.password}  /> */}
 
-                <label htmlFor='address'>address</label>
-                <input id="numHouse" name='numHouse' type='text' defaultValue={user.num_house} required/>
-                <input id="street" name='street' type='text' defaultValue={user.street} required/>
-                <input id="city" name='city' type='text' defaultValue={user.city} required/>
-                <input id="country" name='country' type='text' defaultValue={user.country} required/>
+                    <Label htmlFor='address' value='address' />
+                    <TextInput id="numHouse" name='numHouse' type='text' defaultValue={user.num_house} required />
+                    <TextInput id="street" name='street' type='text' defaultValue={user.street} required />
+                    <TextInput id="city" name='city' type='text' defaultValue={user.city} required />
+                    <TextInput id="country" name='country' type='text' defaultValue={user.country} required />
 
-                <input id="phone" name='phone' type='text' defaultValue={user.phone} required/>
+                    <Label htmlFor='phone' value='phone' />
+                    <TextInput id="phone" name='phone' type='text' defaultValue={user.phone} required />
 
-                <Button type="submit">save changes</Button>
-            </form>
+                    <Button type="submit" className="m-5">save changes</Button>
+                </form>
+
+            </Card>
 
             <React.Fragment>
-            <Modal show={openModal} onClose={()=>setOpenModal(false)}>
-                <Modal.Header />
-                <Modal.Body>
-                    <h3 className="text-indigo-800 text-lg text-center">profile changes were successfully updated!</h3>
-                </Modal.Body>
-                <Modal.Footer><Button onClick={()=>setOpenModal(false)}>ok</Button></Modal.Footer>
-            </Modal>
+                <Modal show={openModal} onClose={() => setOpenModal(false)}>
+                    <Modal.Header />
+                    <Modal.Body>
+                        <h3 className="text-indigo-800 text-lg text-center">profile changes were successfully updated!</h3>
+                    </Modal.Body>
+                    <Modal.Footer><Button onClick={() => setOpenModal(false)}>ok</Button></Modal.Footer>
+                </Modal>
             </React.Fragment>
         </>
     )
